8859b6985b2d968e28668e9d93baad7836cb9c2f
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2022-12-07 14:07:15 -0500

    Add loginPageDefinition cucumber test

---
+++ src/test/java/cucumber_tests/LoginPageDefinitions.java
@@ -0,0 +1,24 @@
+package cucumber_tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageDefinitions {
+    @Given("The login page")
+    public void the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("The user start the app")
+    public void the_user_start_the_app() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Show the email and password input")
+    public void show_the_email_and_password_input() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
