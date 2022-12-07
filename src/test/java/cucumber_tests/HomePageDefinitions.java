800fb3fdbfde38b1478cdeef07885fe6da90ba26
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2022-12-07 17:17:04 -0500

    Add cucumber home page feature and definition

---
+++ src/test/java/cucumber_tests/HomePageDefinitions.java
@@ -0,0 +1,27 @@
+package cucumber_tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomePageDefinitions {
+
+
+    @Given("The home page")
+    public void the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("The user login is correct")
+    public void the_user_login_is_correct() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Redirect to home page")
+    public void redirect_to_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ src/test/resources/project/homepage.feature
@@ -0,0 +1,7 @@
+Feature: Connection
+    Load the page if the user is connected
+
+   Scenario: Show page if user is log
+    Given The home page
+    When The user login is correct
+    Then Redirect to home page
