380b92842070af362f6ef8fd38edcd553b293b08
francoisdomecq <fradomecq@gmail.com>
2022-12-07 17:37:37 -0500

    Unit test login

---
+++ src/main/UserDatabase.java
@@ -0,0 +1,11 @@
+package main;
+
+
+
+public class UserDatabase {
+
+	public boolean isUserCorrect(User toVerify) {
+		return true;
+	}
+
+}
--- src/test/java/unit_tests/TestLogin.java
+++ src/test/java/unit_tests/TestLogin.java
@@ -4,11 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.User;
+import main.UserDatabase;
+
 class TestLogin {
 
+	private UserDatabase userDatabase = new UserDatabase();
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSuccesLogin() {
+		User user = new User("admin", "admin");
+		assertEquals(this.userDatabase.isUserCorrect(user), true);
+	}
+
+	@Test
+	void testFailLogin() {
+		User user = new User("wrongLogin", "wrongPassword");
+		assertEquals(this.userDatabase.isUserCorrect(user), false);
 	}
 
 }
